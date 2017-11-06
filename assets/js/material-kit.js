2f7ec58111690a2ed4a76e24cf9dc133adc7ac10
Doug Clayton <doug@xnegenx.com>
2017-11-05 22:12:36 -0700

    Updated Dashboard / Inbox + Send Modal

--- assets/js/material-kit.js
+++ assets/js/material-kit.js
@@ -129,3 +129,40 @@ function debounce(func, wait, immediate) {
 };
 
 
+// File Selector Control for Sending Faxes
+$(function() {
+    
+      // We can attach the `fileselect` event to all file inputs on the page
+      $(document).on('change', ':file', function() {
+        var input = $(this),
+            numFiles = input.get(0).files ? input.get(0).files.length : 1,
+            label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+        input.trigger('fileselect', [numFiles, label]);
+      });
+    
+      // We can watch for our custom `fileselect` event like this
+      $(document).ready( function() {
+          $(':file').on('fileselect', function(event, numFiles, label) {
+    
+              var input = $(this).parents('.input-group').find(':text'),
+                  log = numFiles > 1 ? numFiles + ' files selected' : label;
+    
+              if( input.length ) {
+                  input.val(log);
+              } else {
+                  if( log ) alert(log);
+              }
+    
+          });
+      });
+      
+    });
+//Tooltips with Bootstrap - Why is this not in the defailt api
+    $(document).ready(function(){
+        $('[data-toggle="tooltip"]').tooltip({
+            placement : 'top',
+            container: 'body'
+        });
+         
+    });
+
